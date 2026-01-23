baddf6af5955e3858a3b2b11e665722208fc9d03
bitress <cyannejustinl.vega12@gmail.com>
2026-01-23 15:13:27 +0800

    added scroll to top

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './routes/AppRoutes';
+import ScrollToTop from "./components/ScrollToTop.tsx";
 
 const App = () => {
   return (
      <Router>
+         <ScrollToTop />
       <AppRoutes />
    </Router>
   );
