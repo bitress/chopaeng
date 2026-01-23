204042b3d4a7d2ed1eb78e38465729dc6e3a3004
bitress <cyannejustinl.vega12@gmail.com>
2026-01-23 22:02:28 +0800

    fix

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,5 @@
-import { useState } from "react";
 import {Link} from "react-router-dom";
 
-interface SearchResult {
-    found: boolean;
-    query: string;
-    results?: {
-        free: string[];
-        sub: string[];
-    };
-    suggestions?: string[];
-    message: string;
-}
 const Home = () => {
     return (
         <>
